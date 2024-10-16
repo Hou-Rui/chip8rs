ef89c7635c1219012c2a1ef4bf45cec6661e904b
Hou Rui <houruinus@gmail.com>
2024-10-16 23:14:39 +0800

    implement more ops

--- src/asm.rs
+++ src/asm.rs
@@ -15,27 +15,35 @@ pub enum Op {
     XOR { reg1: u8, reg2: u8 },
     ADD { reg1: u8, reg2: u8 },
     SUB { reg1: u8, reg2: u8 },
+    SHR { reg1: u8, reg2: u8 },
+    SUBN { reg1: u8, reg2: u8 },
+    SHL { reg1: u8, reg2: u8 },
+    SNE { reg1: u8, reg2: u8 },
+    LDID { addr: u16 },
+    JPA { addr: u16 },
+    //
+    DRW { reg1: u8, reg2: u8, size: u8 },
 }
 
 macro_rules! parse_naaa {
-    ($op:path, $code:expr) => {{
+    ($op: path, $code: expr) => {{
         $op {
             addr: $code & 0x0FFF,
         }
-    }};
+    }}
 }
 
 macro_rules! parse_nxkk {
-    ($op:path, $code:expr) => {{
+    ($op: path, $code: expr) => {{
         $op {
             reg: (($code & 0x0F00) >> 8) as u8,
             value: ($code & 0x00FF) as u8,
         }
-    }};
+    }}
 }
 
 macro_rules! parse_nxyn {
-    ($op:path, $code:expr) => {{
+    ($op: path, $code: expr) => {{
         $op {
             reg1: (($code & 0x0F00) >> 8) as u8,
             reg2: (($code & 0x00F0) >> 4) as u8,
@@ -43,25 +51,52 @@ macro_rules! parse_nxyn {
     }};
 }
 
+macro_rules! parse_nxys {
+    ($op: path, $code: expr) => {{
+        $op {
+            reg1: (($code & 0x0F00) >> 8) as u8,
+            reg2: (($code & 0x00F0) >> 4) as u8,
+            size: ($code & 0x000F) as u8,
+        }
+    }};
+}
+
 impl Op {
-    pub fn new(code: u16) -> Self {
+    pub fn try_from_raw(code: u16) -> Result<Self, u16> {
         match code {
-            0x0E00 => Self::CLS,
-            0x0EEE => Self::RET,
-            0x1000..=0x1FFF => parse_naaa!(Self::JP, code),
-            0x2000..=0x2FFF => parse_naaa!(Self::CALL, code),
-            0x3000..=0x3FFF => parse_nxkk!(Self::SEI, code),
-            0x4000..=0x4FFF => parse_nxkk!(Self::SNEI, code),
-            code if code & 0xF00F == 0x5000 => parse_nxyn!(Self::SE, code),
-            0x6000..=0x6FFF => parse_nxkk!(Self::LDI, code),
-            0x7000..=0x7FFF => parse_nxkk!(Self::ADDI, code),
-            code if code & 0xF00F == 0x8000 => parse_nxyn!(Self::LD, code),
-            code if code & 0xF00F == 0x8001 => parse_nxyn!(Self::OR, code),
-            code if code & 0xF00F == 0x8002 => parse_nxyn!(Self::AND, code),
-            code if code & 0xF00F == 0x8003 => parse_nxyn!(Self::XOR, code),
-            code if code & 0xF00F == 0x8004 => parse_nxyn!(Self::ADD, code),
-            code if code & 0xF00F == 0x8005 => parse_nxyn!(Self::SUB, code),
-            _ => panic!("Unknown opcode {:?}", code),
+            0x0E00 => Ok(Self::CLS),
+            0x0EEE => Ok(Self::RET),
+            0x1000..=0x1FFF => Ok(parse_naaa!(Self::JP, code)),
+            0x2000..=0x2FFF => Ok(parse_naaa!(Self::CALL, code)),
+            0x3000..=0x3FFF => Ok(parse_nxkk!(Self::SEI, code)),
+            0x4000..=0x4FFF => Ok(parse_nxkk!(Self::SNEI, code)),
+            0x5000..=0x5FFF => match code & 0x000F {
+                0x0 => Ok(parse_nxyn!(Self::SE, code)),
+                _ => Err(code),
+            },
+            0x6000..=0x6FFF => Ok(parse_nxkk!(Self::LDI, code)),
+            0x7000..=0x7FFF => Ok(parse_nxkk!(Self::ADDI, code)),
+            0x8000..=0x8FFF => match code & 0x000F {
+                0x0 => Ok(parse_nxyn!(Self::LD, code)),
+                0x1 => Ok(parse_nxyn!(Self::OR, code)),
+                0x2 => Ok(parse_nxyn!(Self::AND, code)),
+                0x3 => Ok(parse_nxyn!(Self::XOR, code)),
+                0x4 => Ok(parse_nxyn!(Self::ADD, code)),
+                0x5 => Ok(parse_nxyn!(Self::SUB, code)),
+                0x6 => Ok(parse_nxyn!(Self::SHR, code)),
+                0x7 => Ok(parse_nxyn!(Self::SUBN, code)),
+                0xE => Ok(parse_nxyn!(Self::SHL, code)),
+                _ => Err(code),
+            },
+            0x9000..=0x9FFF => match code & 0x000F {
+                0x0 => Ok(parse_nxyn!(Self::SNE, code)),
+                _ => Err(code),
+            },
+            0xA000..=0xAFFF => Ok(parse_naaa!(Self::LDID, code)),
+            0xB000..=0xBFFF => Ok(parse_naaa!(Self::JPA, code)),
+            //
+            0xD000..=0xDFFF => Ok(parse_nxys!(Self::DRW, code)),
+            _ => Err(code)
         }
     }
 }
---
+++ src/cpu.rs
@@ -0,0 +1,191 @@
+use std::thread::sleep;
+use std::time::Duration;
+
+use crate::asm::Op;
+use crate::mem::Mem;
+
+const RAM_MAX: usize = 4096;
+const REG_MAX: usize = 16;
+const STACK_MAX: usize = 16;
+const KEYPAD_MAX: usize = 16;
+const VIDEO_WIDTH: u16 = 64;
+const VIDEO_HEIGHT: u16 = 32;
+const VIDEO_MAX: usize = VIDEO_WIDTH as usize * VIDEO_HEIGHT as usize;
+
+const FONTSET_SIZE: usize = 80;
+const DEFAULT_FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
+#[derive(Default)]
+pub struct Cpu {
+    reg: Mem<u8, u8, REG_MAX>,
+    stack: Mem<u8, u16, STACK_MAX>,
+    ram: Mem<u16, u8, RAM_MAX>,
+    video: Mem<u16, bool, VIDEO_MAX>,
+    keypad: Mem<u8, bool, KEYPAD_MAX>,
+    pc: u16,
+    sp: u8,
+    index: u16,
+    delay_timer: u8,
+    sound_timer: u8,
+}
+
+impl Cpu {
+    pub fn new() -> Self {
+        let mut cpu = Self::default();
+        cpu.reset();
+        cpu
+    }
+
+    pub fn reset(&mut self) {
+        self.init_ram();
+        self.pc = 0x200;
+    }
+
+    fn init_ram(&mut self) {
+        const FONT_START_ADDR: u16 = 0x50;
+        for i in 0..FONTSET_SIZE as u16 {
+            self.ram[FONT_START_ADDR + i] = DEFAULT_FONTSET[i as usize];
+        }
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let b1 = self.ram[self.pc];
+        let b2 = self.ram[self.pc + 1];
+        self.pc += 2;
+        u16::from_ne_bytes([b1, b2])
+    }
+
+    fn exec_op(&mut self, op: Op) {
+        match op {
+            Op::CLS => {
+                self.video.clear();
+            }
+            Op::RET => {
+                self.sp -= 1;
+                self.pc = self.stack[self.sp];
+            }
+            Op::JP { addr } => {
+                self.pc = addr;
+            }
+            Op::CALL { addr } => {
+                self.stack[self.sp] = self.pc;
+                self.sp += 1;
+                self.pc = addr;
+            }
+            Op::SEI { reg, value } => {
+                if self.reg[reg] == value {
+                    self.pc += 2;
+                }
+            }
+            Op::SNEI { reg, value } => {
+                if self.reg[reg] != value {
+                    self.pc += 2;
+                }
+            }
+            Op::SE { reg1, reg2 } => {
+                if self.reg[reg1] == self.reg[reg2] {
+                    self.pc += 2;
+                }
+            }
+            Op::LDI { reg, value } => {
+                self.reg[reg] = value;
+            }
+            Op::ADDI { reg, value } => {
+                self.reg[reg] += value; // natural overflow
+            }
+            Op::LD { reg1, reg2 } => {
+                self.reg[reg1] = self.reg[reg2];
+            }
+            Op::OR { reg1, reg2 } => {
+                self.reg[reg1] |= self.reg[reg2];
+            }
+            Op::AND { reg1, reg2 } => {
+                self.reg[reg1] &= self.reg[reg2];
+            }
+            Op::XOR { reg1, reg2 } => {
+                self.reg[reg1] ^= self.reg[reg2];
+            }
+            Op::ADD { reg1, reg2 } => {
+                let result = self.reg[reg1] as u16 + self.reg[reg2] as u16;
+                self.reg[0xF] = if result > 0xFF { 0 } else { 1 };
+                self.reg[reg1] += result as u8;
+            }
+            Op::SUB { reg1, reg2 } => {
+                let (r1, r2) = (self.reg[reg1], self.reg[reg2]);
+                self.reg[0xF] = if r1 > r2 { 1 } else { 0 };
+                self.reg[reg1] -= r2;
+            }
+            Op::SHR { reg1, .. } => {
+                self.reg[0xF] = self.reg[reg1] & 0x1;
+                self.reg[reg1] >>= 1;
+                todo!("SHR variant using both x and y");
+            }
+            Op::SUBN { reg1, reg2 } => {
+                let (r1, r2) = (self.reg[reg1], self.reg[reg2]);
+                self.reg[0xF] = if r1 < r2 { 1 } else { 0 };
+                self.reg[reg1] = r2 - r1;
+            }
+            Op::SHL { reg1, .. } => {
+                self.reg[0xF] = self.reg[reg1] & 0x80;
+                self.reg[reg1] <<= 1;
+                todo!("SHL variant using both x and y");
+            }
+            Op::SNE { reg1, reg2 } => {
+                if self.reg[reg1] != self.reg[reg2] {
+                    self.pc += 2;
+                }
+            }
+            Op::LDID { addr } => {
+                self.index = addr;
+            }
+            Op::JPA { addr } => {
+                let offset = self.reg[0x0] as u16;
+                self.pc = addr + offset;
+            }
+            //
+            Op::DRW { reg1, reg2, size } => {
+                let x = self.reg[reg1] as u16 % VIDEO_WIDTH;
+                let y = self.reg[reg2] as u16 % VIDEO_HEIGHT;
+                for row in 0..size as u16 {
+                    let sprite = self.ram[self.index + row];
+                    for col in 0..8 {
+                        let sprite_pixel = sprite & (0x80 >> col) != 0;
+                        let video_index = y + row * VIDEO_WIDTH + x + col;
+                        if sprite_pixel && self.video[video_index] {
+                            self.reg[0xF] = 1;
+                        }
+                        self.video[video_index] = sprite_pixel;
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            let code = self.fetch();
+            match Op::try_from_raw(code) {
+                Ok(op) => self.exec_op(op),
+                Err(code) => println!("Warning: unknown opcode {:?}", code),
+            }
+            sleep(Duration::from_millis(200));
+        }
+    }
+}
